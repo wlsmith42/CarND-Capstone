d5de3de74256a671066dfa94546fb7a24ddb74b9
micjey <micjey@gmx.de>
2019-03-25 23:43:37 +0000

    Removed build files. Project should now be built again.

--- ros/devel/share/styx_msgs/cmake/styx_msgs-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(styx_msgs_MESSAGE_FILES "/home/student/capstone/CarND-Capstone/ros/src/styx_msgs/msg/TrafficLight.msg;/home/student/capstone/CarND-Capstone/ros/src/styx_msgs/msg/TrafficLightArray.msg;/home/student/capstone/CarND-Capstone/ros/src/styx_msgs/msg/Waypoint.msg;/home/student/capstone/CarND-Capstone/ros/src/styx_msgs/msg/Lane.msg")
-set(styx_msgs_SERVICE_FILES "")
